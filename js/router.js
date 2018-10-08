a3341f9234d6861e6eca928fec0ff21da33ac499
Giordano Gull <giordano.gull@mmp.htwchur.ch>
2018-10-08 12:29:19 +0200

    Router für Exercises hinzugefügt.

--- js/router.js
+++ js/router.js
@@ -1,22 +1,26 @@
-
 // 2. Define some routes
 // Each route should map to a component. The "component" can
 // either be an actual component constructor created via
 // `Vue.extend()`, or just a component options object.
 // We'll talk about nested routes later.
-const routes = [{ 
-    path: '/', 
-    component: 'dashboard-view',
-    /*children: [{
-        path: '/modal',
-        compoent: 'modal-component'
-    }]*/},
-  { path: '/presentation/:id', component: 'presentation-view' }
+const routes = [{
+        path: '/',
+        component: 'dashboard-view',
+        /*children: [{
+            path: '/modal',
+            compoent: 'modal-component'
+        }]*/
+    },
+    {
+        path: '/presentation/:id',
+        component: 'presentation-view'
+    }
+  //{ path: '/exercise/:id', component: 'exercise-view' }
 ]
 
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
 const router = new VueRouter({
-  routes // short for `routes: routes`
+    routes // short for `routes: routes`
 })

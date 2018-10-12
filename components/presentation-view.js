7d41124b504e7db8e77bf71ff0da50685134fb3c
SAMUELLAPTOP\samue <samuel.rhyner@hotmail.com>
2018-10-12 18:26:14 +0200

    Shareability hinzugefügt.

--- components/presentation-view.js
+++ components/presentation-view.js
@@ -11,10 +11,13 @@ Vue.component('presentation-view', {
                 currentPage: 0
             },
             standardNoteText: 'Text mit einem Klick hinzufügen...',
-            currentlyNoteEdit: 0,
-            noteText: '',
-            notesID: 0,
-            notes: {},
+            globalNoteEdit: 0,
+            noteset: [],
+            /* Für die Suche */
+            value: '',
+            suggestionAttribute: 'name',
+            suggestions: [],
+            selectedEvent: ""
         }
     },
     mixins: [
@@ -53,15 +56,44 @@ Vue.component('presentation-view', {
             });
             if (this.pdf.currentPage != $pages) { this.pdf.currentPage = $pages; }
         },
-        openNotesForm: function (index) {
-            this.currentlyNoteEdit = index;
-            this.noteText = this.notes[index];
+        openNotesForm: function (index, id) {
+            // Loop through Notesets to reset currentlyNoteEdit
+            Object.keys(this.noteset).forEach(function (value) {
+                this.noteset[value].currentlyNoteEdit = 0;
+            }.bind(this));
+
+            this.globalNoteEdit = this.noteset[id].notesID;
+            this.noteset[id].currentlyNoteEdit = index;
+            this.noteset[id].noteText = this.noteset[id].data[index];
         },
         getNotes: function () {
             axios.get(this.getAPIURL() + '/get.php?mode=3&id=' + this.$root.dashboardID + '&user=' + this.$root.userID)
             .then((response) => {
-                this.notesID = response.data.notes[0].ID;
-                this.notes = JSON.parse(response.data.notes[0].notes);
+                var $notesetToPush = [];
+                Object.keys(response.data.notes).forEach(function (value) {
+                    var currNote = response.data.notes[value];
+                    
+                    var masked = currNote.notes.replace(/(?:\r\n|\r|\n)/g, '<br>');
+                    var unmasked = JSON.parse(masked);
+                    Object.keys(unmasked).forEach(function (value) {
+                        var find = '<br>';
+                        var re = new RegExp(find, 'g');
+                        unmasked[value] = unmasked[value].replace(re, '\r\n');
+                    });
+                    $notesetToPush.push({
+                        notesID: currNote.ID,
+                        name: currNote.name,
+                        data: unmasked,
+                        allowEdit: true,
+                        currentlyNoteEdit: 0,
+                        noteText: '',
+                        visible: Number(currNote.visible),
+                        style: {
+                            'background-color': '#' + currNote.color
+                        }
+                    });
+                });
+                this.noteset = $notesetToPush;
             })
             .catch(function (error) {
                 console.log("ERROR - Get Notes. Message:");
@@ -72,7 +104,6 @@ Vue.component('presentation-view', {
             axios.get(this.getAPIURL() + '/get.php?mode=4&id=' + this.$route.params.id)
             .then((response) => {
                 this.url = response.data.presentations[0].filePath;
-                console.log(response.data);
             })
             .catch(function (error) {
                 console.log("ERROR - Get Presentation. Message:");
@@ -81,16 +112,18 @@ Vue.component('presentation-view', {
         },
         inputNotes: function (e) {
             if (e.keyCode === 13 && !e.shiftKey) {
+                var id = e.srcElement.attributes['note-set-id'].nodeValue;
                 e.preventDefault();
-                this.notes[this.currentlyNoteEdit] = this.noteText;
-                this.currentlyNoteEdit = 0;
-                this.backupNotes();
+                this.noteset[id].data[this.noteset[id].currentlyNoteEdit] = this.noteset[id].noteText;
+                this.noteset[id].currentlyNoteEdit = 0;
+                this.globalNoteEdit = 0;
+                this.backupNotes(id);
               }
         },
-        backupNotes: function () {
+        backupNotes: function (id) {
             const params = new URLSearchParams();
-            params.append('notesID', this.notesID);
-            params.append('notes', JSON.stringify(this.notes));
+            params.append('notesID', this.noteset[id].notesID);
+            params.append('notes', JSON.stringify(this.noteset[id].data));
             params.append('mode', 2);
             axios.post(this.getAPIURL() + '/update.php', params)
             .catch(function (error) {
@@ -146,6 +179,28 @@ Vue.component('presentation-view', {
                 // PDF loading error
                 console.error(reason);
             });
+        },
+        changed: function() {
+            var that = this
+            this.suggestions = []
+            axios.get(this.getAPIURL() + '/get.php?mode=5&query=' + this.value)
+                .then(function(response) {
+                    response.data.user.forEach(function(a) {
+                        that.suggestions.push(a)
+                    })
+                }).catch(function (fault) {
+                })
+        },
+        shareNotes: function() {
+            axios.get(this.getAPIURL() + '/get.php?mode=5&query=' + this.value)
+            .then(function(response) {
+                console.log(response);
+                if (response.data.message == undefined && value != '') {
+                    alert("gut!");
+                } else {
+                    alert("falsch");
+                }
+            })
         }
     },
     watch: {
@@ -159,6 +214,21 @@ Vue.component('presentation-view', {
             // Speichere das PDF-Objekt
             pdfjsLib.getDocument(this.url).then((pdf) => {this.pdf.pdfObject = pdf;});
         }
+    },
+    computed: {
+        computedNoteset: function () {
+            var $newNoteset = [];
+            
+            Object.keys(this.noteset).forEach(function (value) {
+                if (this.noteset[value].visible == 1) {
+                    $newNoteset.push(this.noteset[value]);
+                }
+            }.bind(this));
+            return $newNoteset;
+        }
+    },
+    components: {
+        'vue-instant': VueInstant.VueInstant
     },
     template: `
     <div class="container">
@@ -182,13 +252,35 @@ Vue.component('presentation-view', {
                         <div class="panel-menu">
                             <button class="toggleFullscreen" v-on:click="toggleFullscreen"><i class="material-icons">fullscreen</i></button> | 
                             Seite {{ pdf.currentPage }} / {{ pdf.totalPages }} | 
-                            <button v-tippy="{ html : '#share-notes'  , interactive : true , placement: 'bottom', theme: 'light' }"><i class="material-icons">share</i></button>
+                            <button v-tippy="{ html : '#share-notes', reactive : true, interactive : true, placement : 'bottom', theme: 'light', trigger: 'click' }"><i class="material-icons">share</i></button>
 
                             <div id="share-notes" x-placement="bottom">
                                 <div class="share-notes-modal">
-                                    <h3> Share Notes</h3>
-                                    <p style="color: black"> Hallo - data binding </p>
-                                    <button>Click</button>
+                                    <h3> Notizensätze</h3>
+                                    <div v-for="noteSetName in noteset">
+                                        <p>
+                                            Set: <b>{{ noteSetName.name }}</b>
+                                            <button v-if="noteSetName.visible === 0" @click="noteSetName.visible = 1">anzeigen</button>
+                                            <button v-else @click="noteSetName.visible = 0">verbergen</button>
+                                            <button v-tippy="{ html : '#sendToUser{{noteSetName.noteID}}', reactive : true, interactive : true, placement : 'bottom', theme: 'light', trigger: 'click' }">Senden</button>
+                                            
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div :id="['sendToUser' + noteSetName.noteID]" x-placement="bottom" v-for="noteSetName in noteset">
+                                <div class="share-notes-name-dialog">
+                                        <vue-instant :suggestion-attribute="suggestionAttribute" 
+                                        v-model="value" 
+                                        :disabled="false" 
+                                        @input="changed" 
+                                        @click-button="shareNotes"
+                                        :show-autocomplete="true"
+                                        :autofocus="false" 
+                                        :suggestions="suggestions" 
+                                        name="customName" 
+                                        placeholder="Name eingeben" 
+                                        type="google"></vue-instant>
                                 </div>
                             </div>
 
@@ -202,22 +294,22 @@ Vue.component('presentation-view', {
                             <div v-if="loading"> lade!</div>
                             
                             <div class="reihe" v-for="pageID in pdf.totalPages" v-if="renderPages">
-                            
-                                <canvas :id="['page-' + pageID]" class="pdf-viewer-page"></canvas>
+
+                                <div class="pdf-page-wrapper">
+                                    <canvas :id="['page-' + pageID]" class="pdf-viewer-page"></canvas>
+                                </div>
                                 <div :id="['note-slide-' + pageID]" class="panel-notes-note">
                                     <h1>Seite {{ pageID }}</h1>
-                                    <div v-on:click="openNotesForm(pageID)" class="text-area-preview" :id="['note-page-' + pageID]" v-if="currentlyNoteEdit !== pageID">
-                                        <p v-if="notes[pageID]">{{ notes[pageID] }}</p>
-                                        <p v-else>{{ standardNoteText }}</p>
-                                    </div>
-                                    <div class="text-area-edit" v-if="currentlyNoteEdit === pageID">
-                                        <form id="notesForm" class="notes-form" action="">
-                                            <textarea rows="8" @keydown="inputNotes" class="note-input-text" v-model="noteText" v-focus></textarea>
-                                            
-                                        </form>
-                                    </div>
-                                    <div class="text-area-preview">
-                                        Testtext
+                                    <div v-for="(satz, id) in computedNoteset"
+                                        v-on:click="openNotesForm(pageID, id)" 
+                                        class="text-area-preview" 
+                                        :style="satz.style"
+                                        :id="['note-page-' + pageID]"><small>{{satz.name}}</small><span v-if="satz.currentlyNoteEdit !== pageID || satz.notesID !== globalNoteEdit">
+                                                <p v-if="satz.data[pageID]">{{ satz.data[pageID] }}</p>
+                                                <p v-else>{{ standardNoteText }}</p>
+                                            </span><form id="notesForm" class="notes-form" action="" v-if="satz.currentlyNoteEdit === pageID && satz.notesID === globalNoteEdit">
+                                                <textarea rows="8" @keydown="inputNotes" :note-set-id="id" class="note-input-text" v-model="satz.noteText" v-focus></textarea>
+                                            </form>
                                     </div>
                                 </div>
                             </div>

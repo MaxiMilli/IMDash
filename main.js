663ff0613a8c0ba2a36ab1abd9273ee46ad2fbc0
SAMUELLAPTOP\samue <samuel.rhyner@hotmail.com>
2018-10-15 07:13:20 +0200

    Sweet Modal hinzugefügt

--- main.js
+++ main.js
@@ -2,23 +2,70 @@
 //const URL = "862341-7.web1.fh-htwchur.ch/api/get.php?mode=1&id=1";
 //const apiURL = "http://862341-7.web1.fh-htwchur.ch/api";
 
-
 Vue.directive('focus', {
     inserted: function (el) {
         el.focus()
     }
 });
+
+Vue.use(Snotify)
+
 var app = new Vue({
     router,
     el: '#app',
+    mixins: [
+        mixinAPI
+    ],
     data: {
         message: 'Hello Vue!',
         viewModal: false,
         viewModalData: {},
         modalID: 5,
-        dashboardID: 1,
-        userID: 1,
+        dashboardID: 0,
+        userID: 0,
+        userDataLoaded: false,
+    },
+    mounted: function () {
+        this.getDataPoint('user', 'active', 1).then(function (response) {
+            var optString = "";
+            Object(response.data).forEach(user => {
+                optString += "<option value='" + user.ID + "'>" + user.name + " (" + user.ID +  ")</option>";
+            });
+            var that = this;
+            $.sweetModal({
+                title: 'User wählen',
+                content: '<select id="userSelection">' + optString + '</select>',
+                blocking: true,
+                buttons: {
+                    accept: {
+                        label: 'Auswählen',
+                        classes: '',
+                        action: function () {
+                            that.setUser(Number($('#userSelection').find(":selected")[0].value));
+                        }
+                    },
+                }
+            });
+        }.bind(this));
     },
     methods: {
+        setUser: function (id) {
+            this.userID = id;
+        },
+    },
+    watch: {
+        userID: function (old, neww) {
+            this.getDataPoint('userDashboard', 'userID', this.userID).then(function (response) {
+                Object(response.data).forEach(user => {
+                    if (user.startup == 1) {
+                        this.dashboardID = user.dashboardID;
+                    }
+                });
+            }.bind(this));
+        },
+        dashboardID: function (old, neww) {
+            this.userDataLoaded = true;
+        }
     }
 });
+

5d5e23b898fd8a0171d439e9f3bbc387fbc94188
SAMUELLAPTOP\samue <samuel.rhyner@hotmail.com>
2018-10-31 17:23:14 +0100

    Vorlage angepasst mit grundlegender HTML-Struktur

--- components/vorlage.js
+++ components/vorlage.js
@@ -9,7 +9,19 @@ Vue.component('dashboard-view', {
         },
     },
     template: `
-    
+    <div class="dashboard-view">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-xs-12">
+                    <span class="title-site">IMDash</span>
+                    <span class="title-name">Übungen</span>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <notification-button></notification-button>                    
+                </div>
+            </div>
+        </div>
+    </div>
     
     `
 });

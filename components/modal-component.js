93091851448bbf0e845ad293aae3e746cd9d087f
Giordano Gull <giordano.gull@mmp.htwchur.ch>
2018-11-13 18:02:05 +0100

    Icons im Modalfenster optimiert / angepasst

--- components/modal-component.js
+++ components/modal-component.js
@@ -35,7 +35,7 @@ Vue.component('modal-component', {
                         <div class="modal-column-head">Vorlesungen</div>
                         <div class="modal-column-list-container">
                             <router-link class="modal-column-list-item" v-for="item in themadata.presentations" :key="item.ID" v-on:click="closeModal" :to="{ path: '/presentation/' + item.ID}">
-                                <i class="material-icons">desktop_windows</i> {{ item.fileName }}
+                                <i class="material-icons">insert_drive_file</i> {{ item.fileName }}
                             </router-link>
                             <span v-if="themadata.presentations.length == 0" class="modal-column-list-noentry"><br><br>Keine Präsentationen vorhanden </span>
                         </div>
@@ -55,13 +55,13 @@ Vue.component('modal-component', {
                         <div class="modal-column-head">Aufzeichnungen</div>
                         <div class="modal-column-list-container">
                             <a class="modal-column-list-item" v-for="item in themadata.stream" v-bind:href="item.link" target="_blank">
-                                <i class="material-icons">code</i> {{ item.name }}
+                                <i class="material-icons">ondemand_video</i> {{ item.name }}
                             </a>
                             <span v-if="themadata.stream.length == 0" class="modal-column-list-noentry"><br><br>Keine Streams vorhanden </span>
                         </div>
                     </div>
                     <div class="modal-column">
-                        <div class="modal-column-head">Dokumente</div>
+                        <div class="modal-column-head">Dokumente <i class="material-icons">file_download</i></div>
                         <div class="modal-column-list-container">
                             <div class="modal-column-list-item" v-for="item in themadata.file" :key="item.ID">
                                 <a :href="item.link" download>
